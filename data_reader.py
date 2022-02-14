e75a6844373003e20c0fae7835289f732697e32e
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 17:10:58 +0330

    create class medical insurance price data

--- data_reader.py
+++ data_reader.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+class InsuranceCostData:
+    _file_name = ''
+
+    def __init__(self, file_name):
+        self._file_name = file_name
+
+    def read_data(self):
+        data = pd.read_csv(self._file_name)
