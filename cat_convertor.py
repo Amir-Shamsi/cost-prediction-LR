d1cffb900a3fd09f014ff68871a9bb73fcfb102f
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-14 17:32:46 +0330

    create function `convert_to_category`

--- cat_convertor.py
+++ cat_convertor.py
@@ -1,1 +1,12 @@
+from typing import Union
 
+from pandas import DataFrame
+from pandas.io.parsers import TextFileReader
+
+
+def convert_to_category(data: Union[TextFileReader, DataFrame]) -> Union[TextFileReader, DataFrame]:
+    """
+    :param data: to convert type to category
+    :return: dataframe
+    """
+    return data.astype('category')
