4bb57cd5f71d5197889852662e14866677705fd0
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 17:22:51 +0330

    write example for `how to use`

--- src/example/insurance_cost.py
+++ src/example/insurance_cost.py
@@ -1,7 +1,22 @@
+from _colors import Mcolor
 from predictor import InsuranceCostPredictor
-
 
 icp = InsuranceCostPredictor('../../dataset/insurance.csv')
 pred_cost = icp.predict(file_name='../../src/example/new_data.csv')
 
 pred_data = icp.get_predict_data()
+
+print('+--------------------------------------------------+')
+print('|\t\t\t\tMade by Amir Shamsi\t\t\t\t   |')
+""" 
+    Github: https://github.com/Amir-Shamsi
+    Linkedin: https://linkedin.com/in/amir-shamsi
+"""
+
+print('+--------------------------------------------------+')
+print(Mcolor.HEADER + Mcolor.BOLD +
+      'data for prediction:\n' + Mcolor.ENDC + Mcolor.OKCYAN
+      , pred_data, Mcolor.ENDC)
+print('+--------------------------------------------------+')
+print(Mcolor.HEADER + 'predicted cost for this data is:', Mcolor.BOLD + Mcolor.OKGREEN + str(pred_cost[0]) + Mcolor.ENDC)
+print('+--------------------------------------------------+')
