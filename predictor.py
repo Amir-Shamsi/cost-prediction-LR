e58ca649196b829787a84d3e75ac7f4a53c19299
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 10:40:45 +0330

    separate `cat type` data based on their values

--- predictor.py
+++ predictor.py
@@ -1,8 +1,12 @@
+import pandas as pd
 from dataset.data_reader import InsuranceCostData
 from cat_convertor import convert_to_category
 
 data_set = InsuranceCostData(file_name='dataset/insurance.csv')
 data_set = data_set.read_data()
+
 convert_to_category(data_set, 'sex')
 convert_to_category(data_set, 'smoker')
 convert_to_category(data_set, 'region')
+
+dum_data = pd.get_dummies(data_set)
