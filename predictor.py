c898fe648bb3f8377683e9f3fcc75488d184408b
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 10:44:19 +0330

    split the train and the test

--- predictor.py
+++ predictor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from dataset.data_reader import InsuranceCostData
 from cat_convertor import convert_to_category
+from sklearn.model_selection import train_test_split
 
 data_set = InsuranceCostData(file_name='dataset/insurance.csv')
 data_set = data_set.read_data()
@@ -13,3 +14,5 @@ dum_data = pd.get_dummies(data_set)
 
 x = dum_data.drop('charges', axis=1)
 y = dum_data['charges']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
