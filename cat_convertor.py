1595711fb584b80a2657fe93618b1ed47aff3180
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 10:01:09 +0330

    get index to change `data_set` type by ref

--- cat_convertor.py
+++ cat_convertor.py
@@ -4,9 +4,10 @@ from pandas import DataFrame
 from pandas.io.parsers import TextFileReader
 
 
-def convert_to_category(data: Union[TextFileReader, DataFrame]) -> Union[TextFileReader, DataFrame]:
+def convert_to_category(data: Union[TextFileReader, DataFrame], index: str):
     """
     :param data: to convert type to category
+    :param index: index of the data we want to change to category
     :return: dataframe
     """
-    return data.astype('category')
+    data[index] = data[index].astype('category')
