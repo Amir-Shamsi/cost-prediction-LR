253007728f7f99969aea7a78481a8821d4d41e96
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 17:41:50 +0330

    add doc for `data_reader` class

--- dataset/data_reader.py
+++ dataset/data_reader.py
@@ -5,15 +5,29 @@ class InsuranceCostData:
     _data = None
 
     def __init__(self, file_name):
+        """
+        :param file_name: to read the data set file *** must be csv ***
+        """
         self._file_name = file_name
 
     def read_data(self):
+        """
+        :return: the data set in form of pandas data frame.
+        """
         self._data = pd.read_csv(self._file_name)
         self._data.head()
         return self._data
 
     def print_info(self):
+        """
+        will print the information of dataframe.
+        :return: None
+        """
         print(self._data.info)
 
     def print_shape(self):
+        """
+        print shape or row and columns of dataframe.
+        :return:
+        """
         print(self._data.shape)
