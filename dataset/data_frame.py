365e2e6cab8ede0e1171c4c109bceb6d0e179484
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 13:30:49 +0330

    change param type to `Index`

--- dataset/data_frame.py
+++ dataset/data_frame.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from pandas import DataFrame
+from pandas import DataFrame, Index
 
 
-def make_data_frame(columns: list[str]) -> DataFrame:
+def make_data_frame(columns: Index) -> DataFrame:
     """
     :param columns: columns to make data frame
     :return: made data frame
