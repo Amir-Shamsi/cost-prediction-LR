92eda8972dcb20f05924999cacc2379e87e95dc0
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 10:38:30 +0330

    convert type of dependent data to category

--- predictor.py
+++ predictor.py
@@ -1,3 +1,8 @@
 from dataset.data_reader import InsuranceCostData
+from cat_convertor import convert_to_category
+
 data_set = InsuranceCostData(file_name='dataset/insurance.csv')
 data_set = data_set.read_data()
+convert_to_category(data_set, 'sex')
+convert_to_category(data_set, 'smoker')
+convert_to_category(data_set, 'region')
