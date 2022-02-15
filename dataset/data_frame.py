37685873e2b9e1c0b0fb6c3d044228d9058e0f77
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 16:16:54 +0330

    update `dummies_data_frame` function

--- dataset/data_frame.py
+++ dataset/data_frame.py
@@ -5,12 +5,16 @@ from pandas import DataFrame, Index
 def dummies_data_frame(columns: Index, av_columns: DataFrame) -> DataFrame:
     """
     :param columns: columns to make data frame
+    :param av_columns: columns that must be ignored
     :return: made data frame
     """
     _structure = {}
     for column in columns:
-        _structure[column] = []
+        if column not in av_columns.columns:
+            _structure[column] = ['0']
+        else:
+            _structure[column] = av_columns[column]
     return pd.DataFrame(
         _structure,
-        index=[],
+        index=[0],
     )
