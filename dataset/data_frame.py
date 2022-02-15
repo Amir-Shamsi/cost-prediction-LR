38c531f6a6be1abaa01855ef285af40fb98c7fe1
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 13:24:47 +0330

    add doc for `make_data_frame` function

--- dataset/data_frame.py
+++ dataset/data_frame.py
@@ -3,6 +3,10 @@ from pandas import DataFrame
 
 
 def make_data_frame(columns: list[str]) -> DataFrame:
+    """
+    :param columns: columns to make data frame
+    :return: made data frame
+    """
     _structure = {}
     for column in columns:
         _structure[column] = []
