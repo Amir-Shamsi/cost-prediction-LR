531123b2551a1d29f42c6b7a335bc3b7e885e632
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-02-15 10:51:19 +0330

    convert code to a class

--- predictor.py
+++ predictor.py
@@ -6,26 +6,34 @@ from cat_convertor import convert_to_category
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-data_set = InsuranceCostData(file_name='dataset/insurance.csv')
-data_set = data_set.read_data()
 
-convert_to_category(data_set, 'sex')
-convert_to_category(data_set, 'smoker')
-convert_to_category(data_set, 'region')
+class InsuranceCostPredictor:
+    _file_name = 'dataset/insurance.csv'
 
-dum_data = pd.get_dummies(data_set)
+    def __init__(self, dataset_filename: str = ''):
+        self._file_name = dataset_filename
 
-x = dum_data.drop('charges', axis=1)
-y = dum_data['charges']
+    def predict(self):
+        data_set = InsuranceCostData(file_name=self._file_name)
+        data_set = data_set.read_data()
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
+        convert_to_category(data_set, 'sex')
+        convert_to_category(data_set, 'smoker')
+        convert_to_category(data_set, 'region')
 
-linearR = LinearRegression()
+        dum_data = pd.get_dummies(data_set)
 
-linearR.fit(x_train, y_train)
+        x = dum_data.drop('charges', axis=1)
+        y = dum_data['charges']
 
-linearR.score(x_test, y_test)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
 
-y_predict = linearR.predict(x_test)
+        linearR = LinearRegression()
 
-math.sqrt(mean_squared_error(y_test, y_predict))
+        linearR.fit(x_train, y_train)
+
+        linearR.score(x_test, y_test)
+
+        y_predict = linearR.predict(x_test)
+
+        math.sqrt(mean_squared_error(y_test, y_predict))
